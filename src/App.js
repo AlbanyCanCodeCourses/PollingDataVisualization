47ea226e8bea0d0b3af158ce0f2609f24f2bc59b
fnikitathomas <f.nikita.thomas@gmail.com>
2019-05-07 13:29:02 -0400

    Updated temporary App.js, added mock data to assets folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BarChart, ScatterPlot, LineChart} from 'react-d3-components'
+import data from './assets/MOCK_DATA_150.json'
 import './App.css';
 
+/**
+ * This file is temporary! It will be modified after routing and logic are set - Nikita
+ */
+
+const style = {
+  fill: '#61dafb',
+  fillOpacity: '0.125'
+}
+
 function App() {
+  let d9 = data.map(s => {
+    return {
+      x: parseFloat(s.current_salary.slice(1)),
+      y: parseFloat(s.previous_salary.slice(1)),
+    }
+  })
+  let dF = d9
+
+  dF.sort((a,b)=> a.x - b.x)
+
+  console.log("dF",dF)
+
+  let dP = {
+    label: 'somethingC',
+    values: [...dF]
+  }
+
+  let d0 = data.map(s => {
+    return {
+      x: parseFloat(s.current_salary.slice(1)),
+      y: parseFloat(s.previous_salary.slice(1)),
+    }
+  })
+
+  let dG = d0
+
+  dG.sort((a,b) => a.x - b.x)
+
+  console.log("dG",dG)
+
+  let dY = [].concat(...(data.map(s => {
+    return [
+      {
+        x: 'somethingA',
+        y: parseFloat(s.previous_salary.slice(1)),
+      },
+      {
+        x: 'somethingB',
+        y: parseFloat(s.current_salary.slice(1)),
+      }
+    ]
+  })))
+
+  let dX = {
+    label: 'somethingA',
+    values: [...dG]
+  }
+
+  let dZ = {
+    label: 'somethingA',
+    values: [...dY]
+  }
+
+  console.log(dX)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BarChart 
+        data={dZ} 
+        width={800}
+        height={600}
+        margin={{ top: 10, bottom: 50, left: 0, right: 10 }}
+      />
+      <hr />
+        <div className="sp">
+        <ScatterPlot
+                data={dX}
+                width={980}
+                height={600}
+                style={style}
+                margin={{top: 10, bottom: 50, left: 60, right: 10}}/>
+        </div>
+      <hr/>
+      <LineChart
+                data={dP}
+                width={1200}
+                height={600}
+                margin={{top: 10, bottom: 50, left: 60, right: 5}}/>
     </div>
   );
 }
