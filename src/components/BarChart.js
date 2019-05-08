1babc4308547b3ad105da4128ec72040f31a785a
Ray <herschel.rey@gmail.com>
2019-05-08 17:48:45 -0400

    added Chart with primary data

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {BarChart, ScatterPlot, LineChart, Tooltip} from 'react-d3-components'
-import data from '../assets/MOCK_DATA_150.json'
+// import data from '../assets/MOCK_DATA_150.json'
 import './App.css';
 import ToolTip from './ToolTip/tooltip'
-import SideNav from './SideNav/SideNav'
+// import SideNav from './SideNav/SideNav'
+import Barchart from './Barchart/barchart'
 
 // var dat0 = [
 //   {
@@ -21,80 +22,83 @@ import SideNav from './SideNav/SideNav'
 //   fillOpacity: '0.125'
 // }
 
-var tooltipScatter = (x, y) =>  {
-  return (
-    data.map((item, index) => {
-      return (
-       <ToolTip  key={index}
-         first_name={data.first_name}
-         last_name={data.last_name}
-         gender={data.gender}
-         starting_salary={data.starting_salary}
-         current_salary={data.current_salary}
-       />
-      )
-    })
-  )
-};
+// var tooltipScatter = (x, y) =>  {
+//   return (
+//     data.map((item, index) => {
+//       return (
+//        <ToolTip  key={index}
+//          first_name={data.first_name}
+//          last_name={data.last_name}
+//          gender={data.gender}
+//          starting_salary={data.starting_salary}
+//          current_salary={data.current_salary}
+//        />
+//       )
+//     })
+//   )
+// };
 
 function App() {
-  let d9 = data.map(s => {
-    return {
-      x: parseFloat(s.previous_salary.slice(1)),
-      y: parseFloat(s.current_salary.slice(1)),
-    }
-  })
-  let dF = d9
+//   let d9 = data.map(s => {
+//     return {
+//       x: parseFloat(s.previous_salary.slice(1)),
+//       y: parseFloat(s.current_salary.slice(1)),
+//     }
+//   })
+//   let dF = d9
 
-  dF.sort((a,b)=> a.x - b.x)
+//   dF.sort((a,b)=> a.x - b.x)
 
-  console.log("dF",dF)
+//   console.log("dF",dF)
 
-  let dP = {
-    label: 'somethingC',
-    values: [...dF]
-  }
+//   let dP = {
+//     label: 'somethingC',
+//     values: [...dF]
+//   }
 
-  let d0 = data.map(s => {
-    return {
-      x: parseFloat(s.current_salary.slice(1)),
-      y: parseFloat(s.previous_salary.slice(1)),
-    }
-  })
+//   let d0 = data.map(s => {
+//     return {
+//       x: parseFloat(s.current_salary.slice(1)),
+//       y: parseFloat(s.previous_salary.slice(1)),
+//     }
+//   })
 
-  let dG = d0
+//   let dG = d0
 
-  dG.sort((a,b) => a.x - b.x)
+//   dG.sort((a,b) => a.x - b.x)
 
-  console.log("dG",dG)
+//   console.log("dG",dG)
   
-  let dY = [].concat(...(data.map(s => {
-    return [
-      {
-        x: 'somethingA',
-        y: parseFloat(s.previous_salary.slice(1)),
-      },
-      {
-        x: 'somethingB',
-        y: parseFloat(s.current_salary.slice(1)),
-      }
-    ]
-  })))
+//   let dY = [].concat(...(data.map(s => {
+//     return [
+//       {
+//         x: 'somethingA',
+//         y: parseFloat(s.previous_salary.slice(1)),
+//       },
+//       {
+//         x: 'somethingB',
+//         y: parseFloat(s.current_salary.slice(1)),
+//       }
+//     ]
+//   })))
 
-  let dX = {
-    label: 'somethingA',
-    values: [...dG]
-  }
+//   let dX = {
+//     label: 'somethingA',
+//     values: [...dG]
+//   }
 
-  let dZ = {
-    label: 'somethingA',
-    values: [...dY]
-  }
+//   let dZ = {
+//     label: 'somethingA',
+//     values: [...dY]
+//   }
 
-  console.log(dX)
+//   console.log(dX)
+
+
   return (
     <div >
-    <SideNav/>
+    <Barchart/>
+    {/* <SideNav/> */}
       {/* <BarChart 
 
         data={dZ} 
@@ -102,7 +106,7 @@ function App() {
         height={600}
         margin={{ top: 10, bottom: 50, left: 0, right: 10 }}
       /> */}
-      <hr />
+      {/* <hr />
         <div className="sp">
         <ScatterPlot
                 data={dX}
@@ -112,7 +116,7 @@ function App() {
                 tooltipHtml={tooltipScatter}
                 margin={{top: 10, bottom: 50, left: 60, right: 10}}/>
         </div>
-      <hr/>
+      <hr/> */}
       {/* <LineChart
                 data={dP}
                 width={1200}
