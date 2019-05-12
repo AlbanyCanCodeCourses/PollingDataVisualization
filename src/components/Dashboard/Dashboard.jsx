67b936056db8ed955962a1288d31888b5b3aebe4
Linda Kovacs <lindaprovenza@yahoo.com>
2019-05-12 12:58:47 -0400

    Changed minor syntax in Dashboard Component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Dashboard() {
 
     return (
-      <div><h1 class="text-center">Dashboard Component</h1></div>
+      <div><h1 className="text-center">Dashboard Component</h1></div>
       );
     }
 
