142efef09bbd891e8d484a0e9529b6fcd360ddd1
Ray <herschel.rey@gmail.com>
2019-05-18 12:14:59 -0400

    added barchart after hard reset

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,15 @@ import './App.css';
 import NavBarContainer from "./NavBar/NavBarContainer";
 import Footer from "./Footer/Footer";
 import LoginContainer from "./Login/LoginContainer";
-import Dashboard from "./Dashboard/Dashboard";
+import Barchart from "./Barchart/Barchart";
 
 function App() {
   return (
     <div>
       <Route path="/" component={NavBarContainer} />
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Barchart
+      } />
         <Route exact path="/login" component={LoginContainer} />
         <Redirect to="/" />
       </Switch>
